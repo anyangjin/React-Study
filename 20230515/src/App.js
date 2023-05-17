cc9deeae0e6429fdc0f1a153445ef54a3b19bbb5
anyangjin <ayz0155@gmail.com>
2023-05-17 16:42:34 +0900

    useEffectFetch exam

--- 20230515/src/App.js
+++ 20230515/src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import './'
-import RouteStudy from './RouterSolve/RouteStudy';
-import RouteSolve from './RouterSolve/RouteSolve';
-import Counter from './HookUseEffect/TryUseEffect';
-import Time from './HookUseEffect/Quiz2';
-import Counter2 from './HookUesRef/TryUseRef';
-import TryUseRefDom from './HookUesRef/TryUseRefDom';
-import TryUseMemo from './HookUseMemo/TryUseMemo';
-
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import "./";
+import RouteStudy from "./RouterSolve/RouteStudy";
+import RouteSolve from "./RouterSolve/RouteSolve";
+import Counter from "./HookUseEffect/TryUseEffect";
+import Time from "./HookUseEffect/Quiz2";
+import Counter2 from "./HookUesRef/TryUseRef";
+import TryUseRefDom from "./HookUesRef/TryUseRefDom";
+import TryUseMemo from "./HookUseMemo/TryUseMemo";
+import App2 from "./App2";
 
 function App() {
   return (
@@ -21,7 +19,8 @@ function App() {
       {/* <Time/> */}
       {/* <Counter2/> */}
       {/* <TryUseRefDom/> */}
-      <TryUseMemo/>
+      <TryUseMemo />
+      <App2 />
     </div>
   );
 }
